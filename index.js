46e35522ca65fc3b18c2cb0a736ea35d4a1b2ef2
Haukur Hlöðversson <haukurhlo18@ru.is>
2020-10-31 18:53:12 +0000

    Added CORS to project

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const uuid = require('uuid');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const create = {
     tmpContacts: [],
